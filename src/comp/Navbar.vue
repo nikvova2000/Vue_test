7236763e3c8eb4cb1317f31d8df25016bc0b3771
Vladimir Nikiforov <nikvova2000@mail.ru>
2022-05-20 19:52:34 +0300

    Test3

--- src/comp/Navbar.vue
+++ src/comp/Navbar.vue
@@ -4,8 +4,8 @@
     <div class="navbar__btns">
       <my-button @click="$router.push('/students')">Студенты с подгрузкой</my-button>
       <my-button @click="$router.push('/sections')">Секции с подгрузкой</my-button>
-      <my-button style="margin-left: 20px" @click="$router.push('/stud')">Студенты ВСЕ</my-button>
-      <my-button style="margin-left: 20px" @click="$router.push('/sec')">Секции ВСЕ</my-button>
+      <my-button style="margin-left: 20px" @click="$router.push('/stud')">Студенты ВСЕ </my-button>
+      <my-button style="margin-left: 20px" @click="$router.push('/sec')">Секции ВСЕ </my-button>
      </div>
   </div>
 </template>
